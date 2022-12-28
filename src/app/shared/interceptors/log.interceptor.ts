c8309b5049bdd5b53629b7734c14710d95297214
predeinnikita <predein697@gmail.com>
2022-12-28 15:28:20 +0500

    timing requests

---
+++ src/app/shared/interceptors/log.interceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { PerfMetricaService } from '../services/perf-metrica.service';
+
+@Injectable()
+export class LogInterceptor implements HttpInterceptor {
+
+  constructor(
+    private _perfMetrica: PerfMetricaService,
+  ) {}
+
+  public intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    this._perfMetrica.group('Время выполнения запроса');
+    this._perfMetrica.time(request.url);
+    
+    return next.handle(request).pipe(
+      tap(() => {
+        this._perfMetrica.endGroup('Время выполнения запроса');
+        this._perfMetrica.endTime(request.url);
+      })
+    );
+  }
+}
