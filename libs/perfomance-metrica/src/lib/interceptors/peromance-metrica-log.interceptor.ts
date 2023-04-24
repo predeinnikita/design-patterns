4c21916fd89435a205b1d30d6bf725afa595fcc2
n.predein@profitbase.ru <n.predein@profitbase.ru>
2023-04-24 16:34:46 +0500

    fix requests logging

--- libs/perfomance-metrica/src/lib/interceptors/peromance-metrica-log.interceptor.ts
+++ libs/perfomance-metrica/src/lib/interceptors/peromance-metrica-log.interceptor.ts
@@ -5,7 +5,7 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import { finalize, Observable } from 'rxjs';
 import { PerfomanceMetricaService } from '../perfomance-metrica.service';
 
 @Injectable()
@@ -18,9 +18,9 @@ export class PerfomanceMetricaLogInterceptor implements HttpInterceptor {
   public intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this._perfMetrica.group('Время выполнения запроса');
     this._perfMetrica.time(request.url);
-    
+
     return next.handle(request).pipe(
-      tap(() => {
+      finalize(() => {
         this._perfMetrica.endGroup('Время выполнения запроса');
         this._perfMetrica.endTime(request.url);
       })
